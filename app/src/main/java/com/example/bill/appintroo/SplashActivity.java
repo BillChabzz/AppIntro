11f6f1eac0914f2822d44255c11500b8ec454567
BillChabzz <chaby9.8@gmail.com>
2015-10-12 18:40:23 +0300

    added splash activity class and xml file

---
+++ app/src/main/java/com/example/bill/appintroo/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.bill.appintroo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by bill on 10/12/15. For testing the splash screen
+ */
+public class SplashActivity extends Activity{
+    //Splash timer
+    private static int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
