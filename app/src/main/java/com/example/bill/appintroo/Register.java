89f62fba87feefaf2be3f92080ea24eb2243333f
BillChabzz <chaby9.8@gmail.com>
2015-10-13 17:34:27 +0300

    created register activity class and xml file

---
+++ app/src/main/java/com/example/bill/appintroo/Register.java
@@ -0,0 +1,14 @@
+package com.example.bill.appintroo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by bill on 10/13/15.
+ */
+public class Register extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
